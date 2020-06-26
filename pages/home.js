d4edba646073fee4381014b6ddc5d79746bd7700
Sean Baier <sean.t.baier@gmail.com>
2020-06-26 18:15:03 -0500

    changes

--- pages/home.js
+++ pages/home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
   return (
     <div>
       <div>
-        <h1 className="dashboard-title">Home</h1>
+        <h1 className="dashboard-title">Home Changed</h1>
       </div>
     </div>
   )
